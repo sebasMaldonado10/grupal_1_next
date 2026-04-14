1861241874080460dc904903807792efe9f37087
sebasMaldonado10 <s.maldonado@itecriocuarto.org.ar>
2026-04-14 19:27:17 -0300

    empeze proyecto

---
+++ src/app/ejemplo/page.jsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hola Tailwind
+      </h1>
+    </main>
+  );
+}
