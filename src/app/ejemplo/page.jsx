04ed0d4c053620fcf57a5296b0da3715e155244a
sebasMaldonado10 <s.maldonado@itecriocuarto.org.ar>
2026-04-15 10:52:50 -0300

    subo una ruta

--- src/app/ejemplo/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Hola Tailwind
-      </h1>
-    </main>
-  );
-}
