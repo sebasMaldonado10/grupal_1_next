66946f12317dc85366484bf664f22122e5570efc
sebasMaldonado10 <s.maldonado@itecriocuarto.org.ar>
2026-04-15 18:29:00 -0300

    home lista

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,66 @@
+import Link from "next/link";
+import React from "react";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <main className="min-h-screen bg-gray-900 text-white p-8">
+      <section className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-10">
+        
+        <div className="md:w-1/2">
+          <h1 className="text-4xl font-bold mb-4">Mundo Gamer</h1>
+
+          <p className="text-lg mb-6">
+            Descubrí juegos, noticias y categorías.
+          </p>
+
+          <div className="flex gap-4 mb-6">
+            <Link
+              href={"/home/juegos"}
+              className="bg-violet-600 px-4 py-2 rounded-lg hover:bg-violet-400"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Ver juegos
+            </Link>
+
+            <Link
+              href={"/home/noticias"}
+              className="bg-violet-600 px-4 py-2 rounded-lg hover:bg-violet-400"
             >
-              Learning
-            </a>{" "}
-            center.
+              Ver noticias
+            </Link>
+          </div>
+
+          <p className="text-gray-300">
+            En Mundo Gamer encontrarás los mejores juegos para disfrutar,
+            diversas categorías y una comunidad muy grande para compartir.
+            ¡Suerte, gamer!
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="md:w-1/2 grid grid-cols-2 gap-4">
+          <Image
+            src="/images/counter.jpg"
+            width={400}
+            height={400}
+            alt="gamer 1"
+            className="rounded-xl object-cover w-full h-[200px]"
+          />
+          <Image
+            src="/images/rocket.jpg"
+            width={300}
+            height={200}
+            alt="gamer 2"
+            className="rounded-xl object-cover w-full h-[200px]"
+          />
+          <Image
+            src="/images/gamer.jpg"
+            width={620}
+            height={200}
+            alt="gamer 3"
+            className="rounded-xl object-cover w-full h-[300px] col-span-2"
+          />
         </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
