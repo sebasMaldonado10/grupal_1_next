3245ac257c92024a6f8d788555c46cc337bed713
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-04-16 12:53:32 -0300

    Actualice el footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -57,39 +57,38 @@ export default function RootLayout({ children }) {
         {children}
 
         <footer className="grid grid-cols-3 px-20 py-20 border border-white/10 bg-[#0b1238] ">  
-          <h1 className="text-3xl font-extrabold text-violet-400">
-            Mundo Gamer
-          </h1>
-    
-          <div className="hidden md:flex flex-col items-center gap-8 text-sm font-medium text-white/80 ">
-            <Link href="/" className="hover:text-violet-400 transition">
-              Inicio
-            </Link>
-            <Link href="/" className="hover:text-violet-400 transition">
-              Contacto
-            </Link>            
-            <Link href="/" className="hover:text-violet-400 transition">
-              Preguntas frecuentes
-            </Link>
-            <a href="#" className="hover:text-violet-400 transition">
-              Términos y condiciones
-            </a>
+          {/* Columna 1 */}
+          <div>
+            <h1 className="text-3xl font-extrabold text-violet-400">Mundo Gamer</h1>
+            <p className="text-sm font-medium text-white/80-">© 2026 Mundo Gamer | Todos los derechos reservados</p>
           </div>
-    
+          
+          {/* Columna 2 */}
+          <div className="flex gap-12 flex-row text-sm font-medium text-white/80 px-10">
+            <div className="flex flex-col gap-4">
+              <Link href="/" className="hover:text-violet-400 transition">Inicio</Link>
+              <Link href="/" className="hover:text-violet-400 transition">Contacto</Link>
+            </div>
+            <div className="flex flex-col gap-4">            
+              <Link href="/" className="hover:text-violet-400 transition">Preguntas frecuentes</Link>
+              <Link href="/" className="hover:text-violet-400 transition">Términos y condiciones</Link>
+            </div>
+          </div>
+          {/* Columna 3 */}
           <div className="flex flex-col items-center gap-3 text-sm font-medium text-white/80 w-full md:w-max mx-auto">
             <h5 className="">NUESTRAS REDES</h5>
             <div className="flex gap-4">
               <Link href="https://instagram.com" className="flex items-center justify-center w-12 h-12 border border-white/20 rounded-xl hover:bg-violet-600/20 hover:text-violet-400 transition-all">
-              <Image 
-                src="/images/imgIns.png"
-                alt="Icono de Instagram"   
-                width={40}
-                height={40}
-              />
+                <Image 
+                  src="/images/logo_inst.png"
+                  alt="Icono de Instagram"   
+                  width={40}
+                  height={40}
+                />
               </Link>
               <Link href="https://facebook.com" className="flex items-center justify-center w-12 h-12 border border-white/20 rounded-xl hover:bg-violet-600/20 hover:text-violet-400 transition-all">
                 <Image 
-                  src="/images/imgFb.png"
+                  src="/images/logo_fb.png"
                   alt="Icono de Facebook"   
                   width={40}
                   height={40}
