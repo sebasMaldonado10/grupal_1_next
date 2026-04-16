eebe679020e38ed78dcb4aeaf2f83940fb91ac9d
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-04-16 13:32:58 -0300

    Termine ruta dinamica

--- src/app/juegos/layout.jsx
+++ src/app/juegos/layout.jsx
@@ -44,15 +44,19 @@ export default function JuegosLayout ({ children }) {
     <div className="flex min-h-screen bg-[#0b1238]">
         <aside className="w-72 py-8 px-6 border-r border-white/10">
             <div>
-                <h1 className="text-4xl font-bold">Juegos</h1>
-                <p>Todas nuestros juegos:</p>
+                <h4>Todos nuestros juegos:</h4>
             </div>
 
             <div className="space-y-6 mt-8">
                 {juegosDestacados.map((juego, index) => (
-                    <div className="border-b pb-6 border-zinc-700" key={index}>
-                        <h3 className="text-xl font-semibold">{juego.titulo}</h3>
-                        
+                    <div className="border-b pb-6 border-white/10" key={index}>
+                        <ul className="mt-2">
+                            <li className="pl-4 cursor-pointer transition-all duration-200 hover:scale-105">
+                                <Link href={`/juegos/${juego.titulo.toLowerCase().replace(/\s+/g,"-")}`}> 
+                                    {juego.titulo} 
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 ))}
             </div>
